12bdf2562a822b9fff0e814d4f472bb337ce38d6
Valerian Saliou <valerian@valeriansaliou.name>
2023-11-06 08:57:57 +0100

    v1.3.1

--- mirage_api/__init__.py
+++ mirage_api/__init__.py
@@ -64,7 +64,7 @@ class Mirage(object):
     # Generate request headers
     headers = {
       "Content-Type": "application/json",
-      "User-Agent": "python-mirage-api/1.3.0",
+      "User-Agent": "python-mirage-api/1.3.1",
       "Authorization": self.__generate_auth()
     }
 
