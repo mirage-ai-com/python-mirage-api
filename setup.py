14f2e3366497e393c5f9deb3805bf8b49ae27ed0
Valerian Saliou <valerian@valeriansaliou.name>
2024-12-06 10:19:40 -0300

    v1.6.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='mirage-api',
-    version='1.5.0',
+    version='1.6.0',
     author=u'Valerian Saliou',
     author_email='valerian@valeriansaliou.name',
     packages=find_packages(),
